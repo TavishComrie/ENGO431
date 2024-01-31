f02654736da77509d620c1de0ae4325a240086e7
dylancon17 <94710755+dylancon17@users.noreply.github.com>
2024-01-31 13:24:47 -0700

    Wrote corrections main

---
+++ ImagePointCorrections.m
@@ -0,0 +1,23 @@
+function [xPrime,yPrime] = ImagePointCorrections(x,y)
+%ImagePointCorrections
+% x and y are provided in fiducial system. Units of mm. May be a vector
+% xPrime and yPrime return corrected coordinates for principal point,
+% radial lens, decentring and atmoshperic factors
+
+    %From calibration sheet
+    xp=-0.006;
+    yp=0.006;
+
+    %Principal point offset correction
+    xBar = x - xp;
+    yBar = y - yp;
+
+    %Find other corrections
+    [xrad,yrad] = findRadialLensCorrection(xBar,yBar); 
+    [xdec,ydec] = DecentringLensDistortion(xBar,yBar); 
+    [xatm,yatm] = findAtmosphericRefractionCorrection(xBar,yBar);
+
+    %Apply corrections
+    xPrime = xBar + xrad + xdec + xatm;
+    yPrime = yBar + yrad + ydec + yatm;
+end
