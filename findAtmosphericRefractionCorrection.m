f06e90a8898674573b3e9f12403c9ebf2c72bb2f
Raymond Lee <raymond.lee2@ucalgary.ca>
2024-01-31 13:53:53 -0700

    Added correction functions

---
+++ findAtmosphericRefractionCorrection.m
@@ -0,0 +1,34 @@
+function [atmoRefrCorrectionX, atmoRefrCorrectionY] = findAtmosphericRefractionCorrection(xBar, yBar)
+    % Summary: the findAtmosphericRefractionCorrection function returns the
+    %          corrections to the atmospheric refraction in the x and y
+    %          coordinates
+    %
+    % Input
+    %       xBar: reduced x-coordinates to the principle point
+    %       yBar: reduced y-coordinates to the principle point
+    % Output
+    %       atmoRefrCorrectionX: the corrections to the atmospheric
+    %                            refraction in the x-coordinate
+    %       atmoRefrCorrectionY: the corrections to the atmospheric
+    %                            refraction in the y-coordinate
+    
+    c = 153.358;    % Calibrated Focal Length [mm]
+
+    % Computing the average elevation of the 
+    h = mean([1090.96, 1086.43, ...
+              1090.50, 1090.65, ...
+              1091.55, 1090.82, ...
+              1083.49, 1092.00]) / 1000;
+    Hh = 751.5 / 1000;  % The flying height of the plane (from Lab 1)
+    H = Hh + h;         % The flying height of the plane from the elevation
+
+    r = sqrt(xBar.^2 + yBar.^2);    % Computing the radial distance
+
+    %  Computing the ARDC Model 
+    K = ((2410 * H) / (H^2 - 6 * H + 250)) - ((2410 * h) / (h^2 - 6 * h + 250)) * (h / H);
+
+    % Computing the corrections to the atmospheric refraction in the x and
+    % y coordinates
+    atmoRefrCorrectionX = -xBar .* (1 + (r.^2 ./ c^2)) .* K;
+    atmoRefrCorrectionY = -yBar .* (1 + (r.^2 ./ c^2)) .* K;
+end
---
+++ findRadialLensCorrection.m
@@ -0,0 +1,26 @@
+function [radCorrectionX, radCorrectionY] = findRadialLensCorrection(xBar,yBar)
+    % Summary: the findRadialLensCorrection function returns the
+    %          corrections to the radial lens distortion in the x and y 
+    %          coordinates
+    %
+    % Input
+    %       xBar: reduced x-coordinates to the principle point
+    %       yBar: reduced y-coordinates to the principle point
+    % Output
+    %       radCoorectionX: the corrections to the radial lens distortion
+    %                       in the x-coordinate
+    %       radCoorectionY: the corrections to the radial lens distortion
+    %                       in the y-coordinate
+
+    % Systematic Radial Distortions
+    k1 = 0.8878E-4;
+    k2 = -0.1528E-7;
+    k3 = 0.5256E-12;
+
+    r = sqrt(xBar.^2 + yBar.^2);    % Computing the radial distance
+
+    % Computing the corrections to the radial distortion in the x and y 
+    % coordinates 
+    radCorrectionX = -xBar .* ((k1 .* r^2) + (k2 .* r^4) + (k3 .* r^6));
+    radCorrectionY = -yBar .* ((k1 .* r^2) + (k2 .* r^4) + (k3 .* r^6));
+end
