11f27472facc8a324a18325c0f1e1cd83073d080
TavishComrie <113808140+TavishComrie@users.noreply.github.com>
2024-02-12 21:54:40 -0700

    Commented

--- Tavish_Points.m
+++ Tavish_Points.m
@@ -3,6 +3,8 @@ close all
 clear 
 
 % %%Lab 2 Affine Transformation
+
+%Entering Fiducial Coordinates in pixels
 fids = [1346.25	-19284.75	1345.75	-19286
 19161	-1470.25	19174.75	-1483.25
 1345	-1471.75	1358.25	-1471.75
@@ -12,6 +14,8 @@ fids = [1346.25	-19284.75	1345.75	-19286
 10254	-966.25	10267	-973
 10254.25	-19788.25	10253.25	-19795.5
 ];
+
+%Entering given fiducial coordinates in mm from calibration certificate
 obsin = [-105.997	-105.995
 106.004	106.008
 -106	106.009
@@ -22,9 +26,11 @@ obsin = [-105.997	-105.995
 0.002	-111.998
 ];
 
-%Normalizes the input data
+%Put the obs in a single column of x,y,x,y variables
 obs = normalize(obsin(:,1),obsin(:,2));
 
+%Put the fiducials for image 27 and image 28 into single columns of
+%x,y,x,y... for each specific image
 fids27 = normalize(fids(:,1),fids(:,2));
 fids28 = normalize(fids(:,3),fids(:,4));
 
@@ -45,12 +51,14 @@ v28 = residuals(A28,xhat28,obs);
 [theta,Sx,Sy,skew] = calcParams(xhat27(1),xhat27(2),xhat27(4),xhat27(5));
 [theta28,Sx28,Sy28,skew28] = calcParams(xhat28(1),xhat28(2),xhat28(4),xhat28(5));
 
-%Adjusts and unnormalizes
+%Adjusts and split x,y,x,y columbs back into two columns of x and y
+%coordinates
 finalcoords1 = adjust(xhat27,fids27);
 pCoords27 = unnorm(finalcoords1);
 finalcoords2 = adjust(xhat28,fids28);
 pCoords28 = unnorm(finalcoords2);
 
+%Splits the residuals into x and y columns
 res27 = unnorm(v27);
 res28 = unnorm(v28);
 
@@ -75,6 +83,7 @@ ylabel('y (mm)')
 title('Image Point Residuals from Image 28')
 print(gcf, '6', '-dpng', '-r300');
 
+%Control and Check points from image 27
 points27 = [11781	-1174
 9616.5	-14501.25
 17840.75	-18026.5
@@ -86,6 +95,7 @@ points27 = [11781	-1174
 14686	-18205.5
 ];
 
+%Control and Check points from image 28
 points28 = [3726	-851
 1953	-14415.5
 10137.25	-17688.75
@@ -96,7 +106,7 @@ points28 = [3726	-851
 4158.25	-17085
 6984	-17948.5
 ];
-
+%Tie points from image 27
 tPoints27 = [14358.25	-4412.00
 19026.75	-4494.75
 9293.00	-9500.50
@@ -105,6 +115,7 @@ tPoints27 = [14358.25	-4412.00
 19133.25	-16965.00
 ];
 
+%Tie points from image 28
 tPoints28 = [6404.25	-4093.75
 11113.00	-4029.25
 1434.75	-9419.50
@@ -113,31 +124,39 @@ tPoints28 = [6404.25	-4093.75
 11400.00	-16602.75
 ];
 
+%Split columns into a single column of x,y,x,y then adjust points using the
+%Affine parameters and then reverted the column back to two columns. For
+%image 27 control and check points.
 normPoints27 = normalize(points27(:,1),points27(:,2));
 adjPoints27 = adjust(xhat27,normPoints27);
 Fpoints27 = unnorm(adjPoints27);
 
-
+%Split columns into a single column of x,y,x,y then adjust points using the
+%Affine parameters and then reverted the column back to two columns. For
+%image 28 control and check points
 normPoints28 = normalize(points28(:,1),points28(:,2));
 adjPoints28 = adjust(xhat28,normPoints28);
 Fpoints28 = unnorm(adjPoints28);
 
+%Extracting Correction information for images 27 and 28 control and check
+%points
 [Xprime27,Yprime27, correctionsMatrix27] = ImagePointCorrections(Fpoints27(:,1),Fpoints27(:,2));
 [Xprime28,Yprime28, correctionsMatrix28] = ImagePointCorrections(Fpoints28(:,1),Fpoints28(:,2));
 
+%Completing the same process as above for tie points of image 27
 normTPoints27 = normalize(tPoints27(:,1),tPoints27(:,2));
 adjTPoints27 = adjust(xhat27,normTPoints27);
 FTpoints27 = unnorm(adjTPoints27);
 [XprimeT27,YprimeT27, correctionsMatrixT27] = ImagePointCorrections(FTpoints27(:,1),FTpoints27(:,2));
 
-
+%Completing the same process as above for tie points of image 28
 normTPoints28 = normalize(tPoints28(:,1),tPoints28(:,2));
 adjTPoints28 = adjust(xhat28,normTPoints28);
 FTpoints28 = unnorm(adjTPoints28);
 [XprimeT28,YprimeT28, correctionsMatrixT28] = ImagePointCorrections(FTpoints28(:,1),FTpoints28(:,2));
 
 %Transformation check with Example from lecture notes
-
+%Following same steps as for the previous transformation of the lab data 
 Comp_cords= [-113.767	-107.400
 -43.717		-108.204
 36.361		-109.132
@@ -174,12 +193,11 @@ Reseaux_Coords = [-110	-110
 
 
 obs2 = normalize((Comp_cords(:,1)),Comp_cords(:,2));
-
 res = normalize(Reseaux_Coords(:,1),Reseaux_Coords(:,2));
 
 
 
- A= makeA(obs2);
+A= makeA(obs2);
 
 xhat = makeXhat(A,res);
 
@@ -199,6 +217,9 @@ xlabel('x (mm)')
 ylabel('y (mm)')
 title('Validation Image Point Residual Quiver Plot')
 print(gcf, '7', '-dpng', '-r300');
+
+%Function to populate the A matrix using the formula described
+% in the lab handout
 function A = makeA(fids)
     A = zeros(size(fids,1),6);
     for i = 1:2:size(fids,1)
@@ -212,15 +233,20 @@ function A = makeA(fids)
     end
 end
 
-
+%Function to find the adjusted parameteres using the formula described
+% in the lab report
 function xhat = makeXhat(A,obs)
    xhat = ((transpose(A)*A)^-1)*transpose(A)*obs;
 end
 
+%Function to find the residuals using the formula described
+% in the lab report
 function v = residuals(A,xhat,obs)
     v = (A*xhat)-obs;
 end
 
+%Function to transform the affine linear parameter variables into
+%non-linear parameters using the equations described in the lab report
 function [theta,Sx,Sy,skew] = calcParams(a,b,c,d)
     theta = atan2(c,a);
     Sx = sqrt(a^2+c^2);
@@ -228,6 +254,9 @@ function [theta,Sx,Sy,skew] = calcParams(a,b,c,d)
     skew = atan2((a*b+c*d),(a*d-b*d));
 end
 
+%Function to split two columns of a matrix into one alternating column,
+%takes two columns as input and interates through to add them in the
+%pattern Col1 then Col2 then Col1 and so on.
 function normal = normalize(col1,col2)
     normal = zeros(size(col1,1)*2,1);
     j=1;
@@ -240,6 +269,10 @@ function normal = normalize(col1,col2)
     end
 end
 
+%Function to reverse the normalize function therefore to take one
+%alternating column and revert it back to two columns. Takes on vector as
+%input and iterates through each row alternating between assigning them to
+%a column
 function reverse = unnorm(res)
     reverse = zeros((size(res,1))/2,2);
     j = 1;
@@ -251,6 +284,10 @@ function reverse = unnorm(res)
     end
 end
 
+%Function to adjust pixel coordinates into mm using the affine
+%transformation parameters solved for previously in this lab. Takes the
+%affine parameters and a vector of coordinates as input and returns a
+%vector of adjusted coordinates.
 function finalcoords = adjust(xhat,cCoords)
     finalcoords = zeros(size(cCoords,1),1);
     
