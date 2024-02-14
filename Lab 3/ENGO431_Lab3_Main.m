9f0085ed7c7aff4be8d14c48bec329e53b7dec53
dylancon17 <94710755+dylancon17@users.noreply.github.com>
2024-02-14 11:27:43 -0700

    inputted

--- Lab 3/ENGO431_Lab3_Main.m
+++ Lab 3/ENGO431_Lab3_Main.m
@@ -4,4 +4,9 @@ close all
 
 %formal xl,yl, xr,yr units of mm
 datacheck = load("relative orientation input data.txt");
-data1(:,1)=[];
+datacheck(:,1)=[];
+
+datamain = load("uofc relative orientation input data.txt");
+
+%LeastSquaresAdustment(datacheck);
+%LeastSquaresAdustment(datamain);
