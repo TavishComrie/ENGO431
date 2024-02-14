850455a042f55db3054d86f81572406dbe5ae853
Raymond Lee <raymond.lee2@ucalgary.ca>
2024-02-14 11:23:29 -0700

    Created performLSA function
    We're going to use this function for the validation and actual process

---
+++ Lab 3/performLeastSquaresAdjustment.m
@@ -0,0 +1,7 @@
+function [xhat, vhat] = performLeastSquaresAdjustment(data)
+    %UNTITLED2 Summary of this function goes here
+    %   Detailed explanation goes here
+    
+    xhat = 0;
+    vhat = 0;
+end
