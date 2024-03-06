721f0f20b52554ffa66cf9e8e2b1d68d7d1d902d
dylancon17 <94710755+dylancon17@users.noreply.github.com>
2024-03-06 11:33:48 -0700

    Created stuff

---
+++ Lab 4/ENGO431_Lab4_Main.m
@@ -0,0 +1,10 @@
+clc
+clear
+close all
+
+checkdata = load('absolute orientation input data.txt');
+
+%FORMAT: ID X_m Y_m Z_m X_o Y_o Z_o
+%UNITS: model coordinates in mm, object coordinates in m
+
+[xhatCheck, residualsCheck, RxCheck, dataPrimeCheck] = performLeastSquaresAdjustment(checkdata, 152.15);
