6ba3cbb2c4bbdcb7fd6b385f2890c3e7606538a3
Raymond Lee <raymond.lee2@ucalgary.ca>
2024-03-20 11:54:51 -0600

    Validation Data Added

--- Lab 5/ENGO431_Lab5_ResectionMain.m
+++ Lab 5/ENGO431_Lab5_ResectionMain.m
@@ -3,8 +3,21 @@ clear
 close all
 
 % Format: 
+% (1) Point Number, 
+% (2) X-Coordinate (Image Space), (3) Y-Coordinate (Image Space), 
+% (4) X-Coordinate (Object Space), (5) Y-Coordinate (Object Space), (6) Z-Coordinate (Object Space)
 resectionImageAndObjectCoords = load("resectionImageAndObjectCoords.txt");
 
+validationImgCoords = resectionImageAndObjectCoords(:, 2:3);
+validationObjCoords = resectionImageAndObjectCoords(:, 4:6);
+
 % Format: 
-resectionEOPs = load("resectionEOPs.txt");
+% (1) Xc, (2) Yc, (3) Zc, (4) w, (5) p, (6) k
+validationResectionEOPs = load("resectionEOPs.txt");
+
+
+% Format: 
+
+
+% Format: 
 
